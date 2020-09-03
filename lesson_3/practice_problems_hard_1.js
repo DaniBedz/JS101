f10f4bf71efd38cb9fa82af2967443d89de04aa8
Dani Bednarski <bedz@Danis-MacBook-Pro.local>
2020-09-03 12:11:32 +1000

    Fixed formatting.

--- lesson_3/practice_problems_hard_1.js
+++ lesson_3/practice_problems_hard_1.js
@@ -1,4 +1,4 @@
-// Question 1
+//? Question 1
 // Will the following functions return the same results?
 
 function first() {
@@ -18,13 +18,13 @@ console.log(second());
 
 // Try to answer without running the code or looking at the solution.
 
-// Answer
-
-// second() will not reach the 'prop 1: "hi there"' piece of code as the engine will add a ; after the return statement,
+//* Answer
+// second() will not reach the 'prop 1: "hi there"' piece of code as the engine
+// will add a ; after the return statement,
 // whereas first() will instead return "undefined".
 
 
-// Question 2
+//? Question 2
 // What does the last line in the following code output?
 
 let object = { first: [1] };
@@ -36,29 +36,33 @@ console.log(object);
 
 // Try to answer without running the code or looking at the solution.
 
-// Answer
-
-// The last line will return "{ first: [1,2] }" as numArray is a reference to the original array and push() modified the
+//* Answer
+// The last line will return "{ first: [1,2] }" as numArray is a reference to
+//  the original array and push() modified the
 // array, so the original object referenced by object is changed.
 
-// If, instead of modifying the original object, we want to modify numArray but not object, we have two options:
+// If, instead of modifying the original object, we want to modify numArray but
+//  not object, we have two options:
 
-// We can declare and initialize numArray with a reference to a copy of the original array:
+// We can declare and initialize numArray with a reference to a copy of the
+// original array:
 
 let object = { first: [1] };
 let numArray = object["first"].slice();
 numArray.push(2);
 
-// We can use Array.prototype.concat(), which returns a new array instead of modifying the original array:
+// We can use Array.prototype.concat(), which returns a new array instead of
+// modifying the original array:
 
 let object = { first: [1] };
 let numArray = object["first"].concat();
 numArray.push(2);
 
-// Question 3
-// Given the following similar sets of code, what will each code snippet print?
+//? Question 3
+// Given the following similar sets of code, what will each
+// code snippet print?
 
-// A)
+//? A)
 
 function messWithVars(one, two, three) {
   one = two;
@@ -76,14 +80,13 @@ console.log(`one is: ${one}`);
 console.log(`two is: ${two}`);
 console.log(`three is: ${three}`);
 
-// Answer
-
+//* Answer
 console.log(`one is: ${one}`); // one is: ["one"]
 console.log(`two is: ${two}`); // two is ["two"]
 console.log(`three is: ${three}`); // three is ["three"]
 
 
-// B)
+//? B)
 
 function messWithVars(one, two, three) {
   one = ["two"];
@@ -101,14 +104,13 @@ console.log(`one is: ${one}`);
 console.log(`two is: ${two}`);
 console.log(`three is: ${three}`);
 
-// Answer
-
+//* Answer
 console.log(`one is: ${one}`); // one is: ["one"]
 console.log(`two is: ${two}`); // two is ["two"]
 console.log(`three is: ${three}`); // three is ["three"]
 
 
-// C)
+//? C)
 
 function messWithVars(one, two, three) {
   one.splice(0, 1, "two");
@@ -126,18 +128,20 @@ console.log(`one is: ${one}`);
 console.log(`two is: ${two}`);
 console.log(`three is: ${three}`);
 
-// Answer
-
+//* Answer
 console.log(`one is: ${one}`); // one is: ["two"]
 console.log(`two is: ${two}`); // two is ["three"]
 console.log(`three is: ${three}`); // three is ["one"]
 
 
-// Question 4
-// Ben was tasked to write a simple javascript function to determine whether an input string is an IP address using 4 dot-separated numbers, e.g., 10.4.5.11. He is not familiar with regular expressions.
+//? Question 4
+// Ben was tasked to write a simple javascript function to determine whether an
+//  input string is an IP address using 4 dot-separated numbers,
+// e.g., 10.4.5.11. He is not familiar with regular expressions.
 
-// Alyssa supplied Ben with a function named isAnIpNumber. It determines whether a string is a numeric string between 0 and 255 
-// as required for IP numbers and asked Ben to use it. Here's the code that Ben wrote:
+// Alyssa supplied Ben with a function named isAnIpNumber. It determines whether
+// a string is a numeric string between 0 and 255 as required for IP numbers
+// and asked Ben to use it. Here's the code that Ben wrote:
 
 function isDotSeparatedIpAddress(inputString) {
   let dotSeparatedWords = inputString.split(".");
@@ -160,13 +164,15 @@ function isAnIpNumber(str) {
   return number >= 0 && number <= 255;
 }
 
-// Alyssa reviewed Ben's code and said, "It's a good start, but you missed a few things. 
-// You're not returning a false condition, and you're not handling the case when the input string has 
-// more or less than 4 components, e.g., 4.5.5 or 1.2.3.4.5: both those values should be invalid."
+// Alyssa reviewed Ben's code and said, "It's a good start,
+// but you missed a few things. 
+// You're not returning a false condition, and you're not handling the case when
+// the input string has more or less than 4 components, e.g., 4.5.5 or
+// 1.2.3.4.5: both those values should be invalid."
 
 // Help Ben fix his code.
 
-// Answer
+//* Answer
 
 function isDotSeparatedIpAddress(inputString) {
   let dotSeparatedWords = inputString.split(".");
--- lesson_3/practice_problems_medium_1.js
+++ lesson_3/practice_problems_medium_1.js
@@ -1,21 +1,21 @@
-// Question 1
+//? Question 1
 // Let's do some "ASCII Art": a stone-age form of nerd artwork from back in
 // the days before computers had video screens.
 // For this practice problem, write a program that creates the following
-//  output 10 times, with each line indented 1 space to the right of the line above it:
+//  output 10 times, with each line indented 1 space to the right of the
+//  line above it:
 
 // The Flintstones Rock!
 //  The Flintstones Rock!
 //   The Flintstones Rock!
 
-// Answer
-
+//* Answer
 for (let padding = 1; padding <= 10; padding++) {
   console.log(' '.repeat(padding) + 'The Flintstones Rock');
 }
 
 
-// Question 2
+//? Question 2
 // Starting with the string:
 
 let munstersDescription = "The Munsters are creepy and spooky.";
@@ -24,8 +24,7 @@ let munstersDescription = "The Munsters are creepy and spooky.";
 
 // `tHE mUNSTERS ARE CREEPY AND SPOOKY.`
 
-// Answer
-
+//* Answer
 munstersDescription.split('').map(function char() {
   if (char === char.toUpperCase()) {
     return char.toLowerCase();
@@ -35,10 +34,11 @@ munstersDescription.split('').map(function char() {
 }).join('');
 
 
-// Question 3
-// Alan wrote the following function, which was intended to return all of the factors of number:
+//? Question 3
+// Alan wrote the following function, which was intended to return all of
+// the factors of number:
 
-// Answer
+//* Answer
 function factors(number) {
   let divisor = number;
   let factors = [];
@@ -50,13 +50,13 @@ function factors(number) {
   }
 }
 
-// Bonus: What is the purpose of number % divisor === 0 in that code?
+//? Bonus: What is the purpose of number % divisor === 0 in that code?
 
-// Answer
+//* Answer
 // If a number modulus a diviser === 0 then it is a factor.
 
 
-// Question 4
+//? Question 4
 // Alyssa was asked to write an implementation of a rolling buffer.
 // You can add and remove elements from a rolling buffer. However,
 // once the buffer becomes full, any new elements will displace the
@@ -67,7 +67,7 @@ function factors(number) {
 // Do you prefer push() or concat() for modifying the buffer?".
 
 // Is there a difference between these implementations,
-//other than the method she used to add an element to the buffer?
+// other than the method she used to add an element to the buffer?
 
 function addToRollingBuffer1(buffer, maxBufferSize, newElement) {
   buffer.push(newElement);
@@ -85,12 +85,12 @@ function addToRollingBuffer2(buffer, maxBufferSize, newElement) {
   return buffer;
 }
 
-// Answer
+//* Answer
+// .push will mutate the buffer arugment,
+// whereas .concat will return a new string.
 
-// .push will mutate the buffer arugment, whereas .concat will return a new string.
 
-
-// Question 5
+//? Question 5
 // What will the following two lines of code output?
 
 console.log(0.3 + 0.6); // 0.999999
@@ -98,33 +98,31 @@ console.log(0.3 + 0.6 === 0.9); // false
 
 // Don't look at the solution before you answer.
 
-// Answer
-// The answers are not as you'd expect due to JavaScript using floating point numbers.
+//* Answer
+// The answers are not as you'd expect due to JavaScript using
+// floating point numbers.
 
 
-// Question 6
+//? Question 6
 // What do you think the following code will output?
 
 let nanArray = [NaN];
 
 console.log(nanArray[0] === NaN); // false
 
-// Answer
-
+//* Answer
 // false
 // NaN cannot be used with a comparator.
 
 
-// Bonus:
-
+//? Bonus:
 // How can you reliably test if a value is NaN?
 
-// Answer
-
+//* Answer
 Number.isNan(number);
 
 
-// Question 7
+//? Question 7
 // What is the output of the following code?
 
 let answer = 42;
@@ -137,11 +135,11 @@ let newAnswer = messWithIt(answer);
 
 console.log(answer - 8);
 
-// Answer
-34; //
+//* Answer
+// 34
 
 
-// Question 8
+//? Question 8
 // One day, Spot was playing with the Munster family's home computer,
 // and he wrote a small program to mess with their demographic data:
 
@@ -173,15 +171,15 @@ messWithDemographics(munsters);
 // His program could replace that with some other object,
 // and the family's data would be safe.
 
-// However, in this case, the program doesn't reassign demoObject; it just uses it, as-is.
+// However, in this case, the program doesn't reassign demoObject;
+// it just uses it, as-is.
 // Thus, the object that gets changed by the function is the munsters object.
 
 
-// Question 9
-
-// Method calls can take expressions as arguments. Suppose we define a function called
-// rps as follows, which follows the classic rules of the rock-paper-scissors game,
-// but with a slight twist: in the event of a tie,
+//? Question 9
+// Method calls can take expressions as arguments. Suppose we define a function
+// called rps as follows, which follows the classic rules of the
+// rock-paper-scissors game, but with a slight twist: in the event of a tie,
 // it awards the win to the first of the two fists.
 
 function rps(fist1, fist2) {
@@ -198,11 +196,11 @@ function rps(fist1, fist2) {
 
 console.log(rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock"));
 
-// Answer
-"Paper"; // Evaluated from outside-in.
+//* Answer
+// "Paper"; // Evaluated from outside-in.
 
 
-// Question 10
+//? Question 10
 // Consider these two simple functions:
 
 function foo(param = "no") {
@@ -217,7 +215,7 @@ function bar(param = "no") {
 
 bar(foo());
 
-// Answer
-"no";
+//* Answer
+// "no";
 // The value returned from the foo function will always be "yes" ,
 // and "yes" === "no" will be false.
