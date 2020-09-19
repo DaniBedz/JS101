8c16786ba2c2168e2a5bdb1b970bd32dc777d343
Dani Bednarski <bedz@Danis-MacBook-Pro.local>
2020-09-19 21:44:28 +1000

    Updated unicode character codes.

--- lesson_6/twentyone.js
+++ lesson_6/twentyone.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync');
-const SUITS = ['\u2764', '\u2756', '\u2660', '\u2663'];
+const SUITS = ['\u2665', '\u2666', '\u2660', '\u2663'];
 const VALUES =
   ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const PICTURE_CARDS = ['A', 'K', 'Q', 'J'];
